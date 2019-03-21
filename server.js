552cef5aabd02afeceb7e2217052229cb9422f64
Mark Likhtar <Mark.Likhtar@itechart-group.com>
2019-03-21 18:53:59 +0300

    add mongodb

--- server.js
+++ server.js
@@ -2,28 +2,41 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const fs = require("fs");
-const path = require("path")
-
-
+const path = require("path");
+const mongoose = require('mongoose');
 const app = express();
-
 const PORT = 3000;
+const Schema = mongoose.Schema;
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+ 
+mongoose.connect('mongodb://localhost/Cinema', function (err) {
+ 
+   if (err) throw err;
+ 
+   console.log('Successfully connected');
+ 
+});
 
-app.get("/api/films", (req, res) => {
-    fs.readFile(path.join(__dirname, "films.json"), "utf-8", (err, data) => {
-        if (err) res.json({
-            result: []
-        })
+const filmScheme = new Schema({
+    name: String,
+    url: String
+});
 
+const Film = mongoose.model("Film", filmScheme);
+
+
+app.get("/api/films", (req, res) => {  
+    Film.find({}, function(err, docs){ 
+        if(err) return console.log(err);
+         
         res.json({
-            result: JSON.parse(data)
+            result: docs
         })
-    })        
+    });
+
         
 })
 
