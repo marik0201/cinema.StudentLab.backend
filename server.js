177e73c1de46dfbaa21a31df20d9c1ccb4bee6d0
Mark Likhtar <Mark.Likhtar@itechart-group.com>
2019-03-22 12:03:41 +0300

    add prettier config

--- server.js
+++ server.js
@@ -12,15 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost/Cinema', function(err) {
-  if (err) { 
-    throw err; 
+  if (err) {
+    throw err;
   }
 });
 
 app.get('/api/films', (req, res) => {
   Film.find({}, function(err, docs) {
-    if (err)  { 
-      return res.status(500).json({ message: "Запрос не выполнен" });
+    if (err) {
+      return res.status(500).json({ message: 'Запрос не выполнен' });
     }
 
     res.json({
