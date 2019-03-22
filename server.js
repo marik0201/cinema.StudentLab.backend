47bebd20ebbdfb811c7a2897b8a26514bdb1e144
Mark Likhtar <Mark.Likhtar@itechart-group.com>
2019-03-22 11:58:31 +0300

    improve errors

--- server.js
+++ server.js
@@ -2,21 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Film = require('./models/Film');
+
 const app = express();
 const PORT = 3000;
-const Film = require('./models/Film');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost/Cinema', function(err) {
-  if (err) throw err;
+  if (err) { 
+    throw err; 
+  }
 });
 
 app.get('/api/films', (req, res) => {
   Film.find({}, function(err, docs) {
-    if (err) return res.status(500).json({ message: "Запрос не выполнен" })
+    if (err)  { 
+      return res.status(500).json({ message: "Запрос не выполнен" });
+    }
 
     res.json({
       result: docs
