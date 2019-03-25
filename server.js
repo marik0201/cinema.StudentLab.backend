428bb49b9f04ecc2b1e5c5b524bbbdc1763ec3e8
Mark Likhtar <Mark.Likhtar@itechart-group.com>
2019-03-25 13:54:21 +0300

    create endpoint to find sesions with film

--- server.js
+++ server.js
@@ -4,30 +4,13 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const Film = require('./models/Film');
 const Session = require('./models/Session');
-var ObjectId = require('mongodb').ObjectId; 
+var ObjectId = require('mongodb').ObjectId;
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-let session = new Session({
-  time: '23-03-2019',
-  cinema: 'Ракета',
-  filmId: '5c93ab0ca8275f39e0e14573'
-});
-
-
-
-  // session.save(function(err, newUser) {
-  //   if (err) {
-  //     console.log('Something goes wrong ');
-  //   } else {
-  //     console.log('Saved');
-  //   }
-  // });
-
 
 mongoose.connect('mongodb://localhost/Cinema', function(err) {
   if (err) {
@@ -48,19 +31,18 @@ app.get('/api/films', (req, res) => {
 });
 
 app.get('/api/sessions/:film', (req, res) => {
-  console.log(req.params.film);
-  
-  Session.find(ObjectId('5c93ab0ca8275f39e0e14573'), function(err, docs){
-     
-    if(err) return console.log(err);
-     
-    console.log(docs);})
 
-    res.json({
-      result: req.params
+  Film.find({ name: req.params.film }, function(err, docs) {
+    if (err) return res.status(500).json({ message: 'Запрос не выполнен' });
+
+    const id = docs[0]._id;
+
+    Session.find({ filmId: id }, function(err, docs) {
+      res.json({
+        result: docs
+      });
     });
- 
+  });
 });
-
 
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
